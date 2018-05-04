1e572770373b22824fd7f74018ad67eb4c23a027
dan wessels <daniel.wessels@levvel.io>
2018-05-04 11:29:22 +1000

    first commit

--- datastore_2017_06_08/CPA/Infrastructure/VM/Provisioning/StateMachines/Methods.class/__methods__/vmware_postprovision.rb
+++
@@ -1,8 +0,0 @@
-prov = $evm.root["miq_provision"]
-vm = $evm.root['vm']
-
-puts "POST_PROV"
-
-$evm.instantiate '/ObjectWalker/ObjectWalker/objectwalker'
-
-
--- datastore_2017_06_08/CPA/Service/Provisioning/StateMachines/ServiceProvisionRequestApproval.class/__methods__/validate_request.rb
+++
@@ -1,2 +0,0 @@
-$evm.instantiate '/Integration/ITSM/BmcRemedy/StateMachines/ChangeCreate'
-exit MIQ_ABORT unless $evm.root['ae_result'] == 'ok'
